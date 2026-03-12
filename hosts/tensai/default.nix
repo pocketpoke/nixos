cfd7290445f86a9567c2532eb3b2e37b8e7dd6a1
pocketpoke <191191762+pocketpoke@users.noreply.github.com>
2026-03-11 20:15:25 -0400

    Cleanup unnecessary comments

--- hosts/tensai/default.nix
+++ hosts/tensai/default.nix
@@ -4,28 +4,13 @@
   imports = [
     ./hardware-configuration.nix
 
-    # Core
     ../../modules/core
-
-    # Boot & storage
     ../../modules/boot
-
-    # Hardware features
     ../../modules/hardware
-
-    # Desktop / GUI
     ../../modules/desktop
-
-    # Networking
     ../../modules/networking
-
-    # Services & programs
     ../../modules/services
-
-    # Shell
     ../../modules/shell
-
-    # Users
     ../../modules/users
   ];
 
