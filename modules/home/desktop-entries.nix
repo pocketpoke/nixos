19384259747b2d6253fc798ae1e4b728d4b7ae2f
pocketpoke <191191762+pocketpoke@users.noreply.github.com>
2026-04-26 15:20:21 -0400

    Fix Hash

--- modules/home/desktop-entries.nix
+++ modules/home/desktop-entries.nix
@@ -5,7 +5,7 @@
     exec = "affinity-v3 %U";
     icon = pkgs.fetchurl {
       url = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/affinity-studio-icon.svg";
-      sha256 = "sha256-0ks42ml9zpmg1lf1cj4g3azc3xac7z3fav5bwsd72j7b9hcgl1";
+      sha256 = "0ks42ml9zpmg1lf1cj4g3azclc3xac7z3fav5bwsd72j7b9hcgl1";
     };
     terminal = false;
     categories = [ "Graphics" ];
